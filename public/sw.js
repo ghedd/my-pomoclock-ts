201648ee5d13377297c978d40cd53db549c29826
eddielewis-thinhle <eddie.thinhle@outlook.com>
2021-02-11 12:36:57 -0500

    update: temporarily turn off sw.js & copy code/logic from there to service-worker.ts for later build version

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 /* eslint-disable no-restricted-globals */
 
 // install service worker
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -14,6 +14,13 @@ import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 
+//NOTE the following interface
+//    attempts to add more prpoperties
+//    to Client interface
+
+interface MatchingClient extends Client {
+  focus?(): any | null;
+}
 declare const self: ServiceWorkerGlobalScope;
 
 clientsClaim();
@@ -22,59 +29,98 @@ clientsClaim();
 // Their URLs are injected into the manifest variable below.
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute( self.__WB_MANIFEST );
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
-const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const fileExtensionRegexp = new RegExp( '/[^/?]+\\.[^/]+$' );
 registerRoute(
   // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }: { request: Request; url: URL }) => {
+  ( { request, url }: { request: Request; url: URL } ) => {
     // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
+    if ( request.mode !== 'navigate' ) {
       return false;
     }
 
     // If this is a URL that starts with /_, skip.
-    if (url.pathname.startsWith('/_')) {
+    if ( url.pathname.startsWith( '/_' ) ) {
       return false;
     }
 
     // If this looks like a URL for a resource, because it contains
     // a file extension, skip.
-    if (url.pathname.match(fileExtensionRegexp)) {
+    if ( url.pathname.match( fileExtensionRegexp ) ) {
       return false;
     }
 
     // Return true to signal that we want to use the handler.
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  createHandlerBoundToURL( process.env.PUBLIC_URL + '/index.html' )
 );
 
 // An example runtime caching route for requests that aren't handled by the
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+  ( { url } ) => url.origin === self.location.origin && url.pathname.endsWith( '.png' ),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new StaleWhileRevalidate({
+  new StaleWhileRevalidate( {
     cacheName: 'images',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
-      new ExpirationPlugin({ maxEntries: 50 }),
+      new ExpirationPlugin( { maxEntries: 50 } ),
     ],
-  })
+  } )
 );
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+self.addEventListener( 'message', ( event ) => {
+  if ( event.data && event.data.type === 'SKIP_WAITING' ) {
     self.skipWaiting();
   }
-});
+} );
 
 // Any other custom service worker logic can go here.
+// install service worker
+self.addEventListener( "install", ( event ) => {
+  console.log( "SW has been installed" );
+} );
+// active service worker
+self.addEventListener( "activate", ( event ) => {
+  console.log( "SW has been activated" );
+} );
+// click event
+self.addEventListener( "notificationclick", ( event ) => {
+  event.notification.close();
+  const urlToOpen = new URL( "/", self.location.origin ).href;
+
+  // @ts-ignore
+  const { clients } = self;
+  const promiseChain = clients
+    .matchAll( {
+      type: "window",
+      includeUncontrolled: true,
+    } )
+    .then( ( windowClients ) => {
+      let matchingClient: MatchingClient | null = null;
+
+      for ( let i = 0; i < windowClients.length; i++ ) {
+        const windowClient = windowClients[i];
+        if ( windowClient.url === urlToOpen ) {
+          matchingClient = windowClient;
+          break;
+        }
+      }
+      if ( matchingClient && matchingClient.focus ) {
+        return matchingClient.focus();
+      } else {
+        return clients.openWindow( urlToOpen );
+      }
+    } );
+
+  event.waitUntil( promiseChain );
+} );
